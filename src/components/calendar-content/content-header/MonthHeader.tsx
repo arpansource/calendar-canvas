2f80d0dde6a869ef32ea5fd8dd9cfcbecf904406
abhishekprajapati1 <ap0661236@gmail.com>
2025-01-27 13:09:56 +0530

    :sparkles: ENHANCEMENT: add className support in MonthHeader component

--- src/components/calendar-content/content-header/MonthHeader.tsx
+++ src/components/calendar-content/content-header/MonthHeader.tsx
@@ -2,7 +2,9 @@ import { MonthContentHeaderProps } from "../../../types";
 
 const MonthHeader: React.FC<MonthContentHeaderProps> = ({ className = "" }) => {
   return (
-    <div className="month-content-header grid grid-cols-7 w-full">
+    <div
+      className={`month-content-header grid grid-cols-7 w-full ${className}`}
+    >
       {["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"].map((d, index) => (
         <div
           className="col-span-1 border grid place-content-center"
